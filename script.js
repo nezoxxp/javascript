59a12ef770af986d9628b78497c3dc83a6efc087
nezox <nezox@gm.com>
2023-06-22 20:35:56 +0500

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+var userName =prompt('What is your name?')
+var age = prompt('How old are you?')
+var pr1 = +prompt('56-42=?')
+var pr2 = +prompt('51+32=?')
+var pr3 = +prompt('9*7=?')
+var pr4 = +prompt('36/3=?')
+var pr5 = +prompt('56%6=?')
+
+console.log(userName);
+console.log(age);    
+console.log('Пример 1: 56-42=' + "(ваш ответ " + pr1 +")");           
+console.log('Пример 1: 51+32=' + "(ваш ответ " + pr2 +")");           
+console.log('Пример 1: 9*7=' + "(ваш ответ " + pr3 +")");           
+console.log('Пример 1: 36/3=' + "(ваш ответ " + pr4 +")");           
+console.log('Пример 1: 56%6=' + "(ваш ответ " + pr5 +")");    
+console.log("");       
